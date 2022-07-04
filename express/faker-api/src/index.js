9c31dc54ada1f1ba8a7049a45b03b2d525308055
Josh Burlile <josh.burlile@gmail.com>
2022-07-04 00:12:05 -0700

    faker api v1.1

--- express/faker-api/src/index.js
+++ express/faker-api/src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const faker = require("faker");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const express = require("express");
+const app = express();
+
+const createUser = () => ({
+    lastName: faker.name.lastName(),
+    firstName: faker.name.firstName(),
+    email: faker.internet.email(),
+    phoneNumber: faker.phone.phoneNumber(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    _Id: faker.datatype.uuid(),
+    });
+
+const createCompany = () => ({
+    _Id: faker.datatype.uuid(),
+    name: faker.company.companyName(),
+    address: {
+        street: faker.address.streetAddress(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zipCode: faker.address.zipCode(),
+        country: faker.address.country(),
+    }
+    });
+
+app.get("/api/users/new", (req, res) => {
+    const newUser = createUser();
+    res.json(newUser);
+});
+
+app.get("/api/companies/new", (req, res) => {
+    const newCompany = createCompany();
+    res.json(newCompany);
+});
+
+app.get("/api/user/company", (req, res) => {
+    const newUser = createUser();
+    const newCompany = createCompany();
+    const responseObject = {
+    user: newUser,
+    company: newCompany,
+    };
+    res.json(responseObject);
+});
+
+const server = app.listen(8000, () =>
+    console.log(`Server is locked and loaded on port ${server.address().port}!`)
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
