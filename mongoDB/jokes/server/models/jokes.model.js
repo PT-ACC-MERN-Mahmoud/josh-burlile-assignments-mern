cd44107a66fd59627d508720db60041c814ba82c
Josh Burlile <josh.burlile@gmail.com>
2022-07-10 22:06:33 -0700

    jokes api v1.0

---
+++ mongoDB/jokes/server/models/jokes.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const JokeSchema = new mongoose.Schema(
+    {
+        setup: String,
+        punchline: String,
+    },
+);
+
+const Joke = mongoose.model('Joke', JokeSchema);
+
+module.exports = Joke;
